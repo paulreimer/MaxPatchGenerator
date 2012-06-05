18d87511b25464fcf8936a77fd656951467d0776
Paul Reimer <paulreimer@gmail.com>
2012-06-05 11:11:41 -0700

    Added default, empty custom.py. At a minimum, your protocol buffer definition should be added and instantiated in this file.

---
+++ custom.py
@@ -0,0 +1,21 @@
+"""
+Copyright Paul Reimer, 2012
+
+This work is licensed under the Creative Commons Attribution-NonCommercial 3.0 Unported License.
+To view a copy of this license, visit
+http://creativecommons.org/licenses/by-nc/3.0/
+or send a letter to
+Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
+"""
+
+ignore_types = [
+  ]
+composite_types = [
+  ]
+
+osc_root_path = ['']
+
+#import XXX_pb2
+#pb = XXX_pb2.ui()
+pb = None
+
